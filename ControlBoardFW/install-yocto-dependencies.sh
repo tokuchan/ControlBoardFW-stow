a0a19731bf8cec25316c683af3ee2d8a1a369d2f
Sean R. Spillane <tokuchan@gmail.com>
2024-06-13 12:33:16 -0400

    feat(yocto): Capture yocto dependencies

---
+++ ControlBoardFW/install-yocto-dependencies.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+sudo apt install -yqq \
+  git \
+  cmake \
+  gcc \
+  g++ \
+  gdb \
+  emacs \
+  gparted \
+  gitk \
+  git-lfs \
+  lib32stdc++6 \
+  libgtk2.0-0 \
+  dpkg-dev \
+  python3-pip \
+  libtinfo5 \
+  libncurses5 \
+  libtinfo6 \
+  libtinfo-dev \
+  gawk \
+  xterm \
+  autoconf \
+  libtool \
+  texinfo \
+  gcc-multilib \
+  net-tools \
+  repo \
+  pv \
+  libi2c-dev \
+  chrpath \
+  diffstat \
+  zstd \
+  lz4 \
+  libgpiod-dev \
+  libssl-dev \
+  gnuplot \
+  stow \
+  rsync \
+  bc \
+  build-essential \
+  language-pack-en
+
+pip install --upgrade --user PyYAML
